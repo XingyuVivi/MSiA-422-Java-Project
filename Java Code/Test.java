7bd2c32ef98cb485651148393a61c3994a9246be
Xingyu Wang <xingyuwang2023@u.northwestern.edu>
2022-12-06 16:02:02 -0600

    slicing, filtering, indexing

--- Java Code/Test.java
+++ Java Code/Test.java
@@ -3,10 +3,79 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
 //      Test path
-        String path_r = "C:\\Users\\nuke2\\Desktop\\NW Work\\Data\\babynames\\IL.TXT";
-        String path_w = "C:\\Users\\nuke2\\Desktop\\NW Work\\MSiA-422-Java-Project\\text_write.txt";
+        String path_r = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/Project/namesbystate/IL.TXT";
+        String path_w = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project" +
+                "/text_write.txt";
         myDataFrame data = MyPandas.readCSV(path_r);
 
-        MyPandas.writeCSV(path_w, data);
+        // Test case1:  myDataFrame slice(int index)
+        myDataFrame output1 = data.slice(3);
+        String p1 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test1.txt";
+        MyPandas.writeCSV(p1, output1);
+
+        // Test case2 for myDataFrame slice(String name)
+        myDataFrame output2 = data.slice("Name");
+        String p2 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test2.txt";
+        MyPandas.writeCSV(p2, output2);
+
+        // Test case3 for myDataFrame slice(int[] indexArr)
+        myDataFrame output3 = data.slice(new int[] {0,1,3});
+        String p3 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test3.txt";
+        MyPandas.writeCSV(p3, output3);
+
+        // Test case4 for myDataFrame slice(String[] nameArr)
+        myDataFrame output4 = data.slice(new String[] {"State","Gender","Name"});
+        String p4 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test4.txt";
+        MyPandas.writeCSV(p4, output4);
+
+        // Test case 5 for myDataFrame filter(String col, String op, Object o)
+
+        //Test for numeric columns
+        myDataFrame output5 = data.filter("Count", "<=", 10);
+        String p5 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test5.txt";
+        MyPandas.writeCSV(p5, output5);
+
+        //Test for categorical columns
+        myDataFrame output5_1 = data.filter("Name", "<=", "Anna");
+        String p5_1 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test5_1.txt";
+        MyPandas.writeCSV(p5_1, output5_1);
+
+
+
+        // Test case 6 myDataFrame loc(int index)
+        myDataFrame output6 = data.loc(3);
+        String p6 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test6.txt";
+        MyPandas.writeCSV(p6, output6);
+
+        // Test case 7 for myDataFrame loc(String label)
+        myDataFrame output7 = data.loc("Aleta");
+        String p7 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test7.txt";
+        MyPandas.writeCSV(p7, output7);
+
+        // Test case 8 for myDataFrame loc(int from, int to)
+        myDataFrame output8 = data.loc(0,10);
+        String p8 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test8.txt";
+        MyPandas.writeCSV(p8, output8);
+
+        //Test case 9 for myDataFrame loc(String from, String to)
+        myDataFrame output9 = data.loc("Agnes","Alison");
+        String p9 = "/Users/wangxingyu/Dropbox/Mac/Desktop/NU/py_Java/MSiA-422-Java-Project/testCase_output" +
+                "/test9.txt";
+        MyPandas.writeCSV(p9, output9);
+
+
+
+
+
+
     }
 }
--- Java Code/myDataFrame.java
+++ Java Code/myDataFrame.java
@@ -37,4 +37,193 @@ public class myDataFrame {
         }
         return new myDataFrame(temp);
     }
+
+    public String dType(int index){
+        //Returns the type of the column specified by index. If the type is not uniform, return ‘String’.
+
+        return "String";
+    }
+
+    public String dType(String name){
+        //Returns the type of the column specified by name. If the type is not uniform, return ‘String’.
+
+        return "String";
+    }
+
+// slicing
+    public myDataFrame slice(int index){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        for (int i = 0; i < this.data.size(); i ++){
+            ArrayList<Object> col_data = new ArrayList<Object>();
+            col_data.add(this.data.get(i).get(index));
+            output_data.add(col_data);
+        }
+        return new myDataFrame(output_data);
+    }
+
+    public myDataFrame slice(String name){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        int index = this.header.indexOf(name);
+        for (int i = 0; i < this.data.size(); i ++){
+            ArrayList<Object> col_data = new ArrayList<Object>();
+            col_data.add(this.data.get(i).get(index));
+            output_data.add(col_data);
+        }
+        return new myDataFrame(output_data);
+    }
+
+    public myDataFrame slice(int[] indexArr){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        for (int i = 0; i < this.data.size(); i ++){
+            ArrayList<Object> col_data = new ArrayList<Object>();
+            for (int j = 0; j < indexArr.length; j++){
+                int index = indexArr[j];
+                col_data.add(this.data.get(i).get(index));
+            }
+            output_data.add(col_data);
+        }
+        return new myDataFrame(output_data);
+    }
+
+    public myDataFrame slice(String[] nameArr){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        for (int i = 0; i < this.data.size(); i ++){
+            ArrayList<Object> col_data = new ArrayList<Object>();
+            for (int j = 0; j < nameArr.length; j++){
+                int index = this.header.indexOf(nameArr[j]);
+                col_data.add(this.data.get(i).get(index));
+            }
+            output_data.add(col_data);
+        }
+        return new myDataFrame(output_data);
+    }
+
+// filtering
+    public myDataFrame filter(String col, String op, Object o){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        int index = this.header.indexOf(col);
+
+
+        // If values in the column are integers
+        if(this.data.get(0).get(index) instanceof Integer && o instanceof Integer){
+
+            // loop through rows of data to see which fulfill the requirement
+            for (int i = 0; i < this.data.size(); i ++ ) {
+                int df_value = (Integer) this.data.get(i).get(index);
+                int target = (Integer) o;
+
+                if (op.equals(">") && df_value > target) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals(">=") && df_value >= target) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals("<") && df_value < target) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals("<=") && df_value <= target) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals("=") && df_value == target) {
+                    output_data.add(this.data.get(i));
+                }
+            }
+        }
+
+        // if values in the column is not an integer, which means it is a string
+        else {
+
+            // loop through rows to see which fulfill the requirement
+            for (int i = 0; i < this.data.size(); i++){
+                String df_value = (String) this.data.get(i).get(index);
+                String target = (String) o;
+                if (op.equals("=") && df_value.compareTo(target) == 0){
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals(">") && df_value.compareTo(target) > 0) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals(">=") && df_value.compareTo(target) >= 0) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals("<") && df_value.compareTo(target) < 0) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals("<=") && df_value.compareTo(target) <= 0) {
+                    output_data.add(this.data.get(i));
+                }
+                else if (op.equals("!=") && df_value.compareTo(target) != 0) {
+                    output_data.add(this.data.get(i));
+                }
+            }
+
+        }
+
+        return new myDataFrame(output_data);
+
+        }
+// indexing
+    public myDataFrame loc(int index){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        output_data.add(this.data.get(index));
+        return new myDataFrame(output_data);
+    }
+
+    public myDataFrame loc(String label){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        // Assume to locate rows by Name
+        int index = this.header.indexOf("Name");
+        for (int i = 0; i < this.data.size(); i++){
+            if (this.data.get(i).get(index).equals(label)){
+                output_data.add(this.data.get(i));
+            }
+        }
+        return new myDataFrame(output_data);
+    }
+
+    public myDataFrame loc(int from, int to){
+        //indexing range from 0 to rows-1 of the dataset
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        for (int i = from; i <= to; i++){
+            output_data.add(this.data.get(i));
+        }
+        return new myDataFrame(output_data);
+    }
+
+    public myDataFrame loc(String from, String to){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        ArrayList<ArrayList<Object>> empty_data = new ArrayList<ArrayList<Object>>();
+        int index = this.header.indexOf("Name");
+        // whether the start name is found.
+        boolean start_found = false;
+        //whether the end name is found
+        boolean end_found = false;
+        for (int i = 0; i < this.data.size(); i++){
+            if (this.data.get(i).get(index).equals(from)){
+                start_found = true;
+            }
+            if (this.data.get(i).get(index).equals(to)){
+                end_found = true;
+                output_data.add(this.data.get(i));
+                break;
+            }
+            if (start_found && !end_found){
+                output_data.add(this.data.get(i));
+            }
+        }
+        if (!start_found || !end_found){
+            //if start name or end name is not found, return an empty dataframe
+            return new myDataFrame(empty_data);
+        }
+
+        return new myDataFrame(output_data);
+    }
+
+    // sorting
+    public myDataFrame sort(int index){
+        ArrayList<ArrayList<Object>> output_data = new ArrayList<ArrayList<Object>>();
+        return new myDataFrame(output_data);
+    }
+
+
 }
